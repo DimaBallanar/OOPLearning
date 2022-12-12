846dde82d30e0e43f3f63f20e2320e61dabc1cfb
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-12 23:26:12 +0300

    старт ооп

---
+++ Work12.12/MyClasses/CustomArray.cs
@@ -0,0 +1,76 @@
+namespace OOPday1.MyClasses
+{
+    public class CustomArray
+    {
+        private int[] Items;
+
+
+        public CustomArray()
+        {
+            Items = new int[0];
+        }
+        public CustomArray(int length)
+        {
+            Items = new int[length];
+        }
+        public CustomArray(int[] array)
+        {
+            Items = array;
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine(this);
+        }
+
+        public void InicializationArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < Items.Length; i++)
+            {
+                Items[i] = rnd.Next(-1000, 1000);
+            }
+        }
+
+        public int AddItem(params int[] values)
+        {
+            int[] arrays = new int[values.Length + Items.Length];
+            Array.Copy(Items, arrays, Items.Length);
+            for (int i = Items.Length; i < arrays.Length; i++)
+            {
+                arrays[i] = values[i - Items.Length];
+
+            }
+
+            Items = arrays;
+            return Items.Length;
+        }
+        public void AddItem(int value, int index)
+        {
+            if (index < 0 || index >= Items.Length)
+            {
+                System.Console.WriteLine("неверный индекс");
+                return;
+            }
+            Items[index] = value;
+        }
+        public int? GetItem(int index)
+        {
+            if (index < 0 || index >= Items.Length)
+            {
+                System.Console.WriteLine("неверный индекс");
+                return null;
+            }
+            return Items[index];
+        }
+
+        public override string ToString()
+        {
+            return string.Join(",", Items);
+        }
+        public static void Information()
+        {
+            System.Console.WriteLine("Myclasses includes Array");
+        }
+    }
+}
