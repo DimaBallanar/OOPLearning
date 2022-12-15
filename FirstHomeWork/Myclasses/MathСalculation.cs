ab271cdc3b4a8589fd8058fff323be5263a53259
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-15 10:13:13 +0300

    3 task

---
+++ FirstHomeWork/Myclasses/MathСalculation.cs
@@ -0,0 +1,17 @@
+// У вас есть класс MathСalculation. Создать в нем функцию для подсчета всех
+// положительных чисел. Проверки
+
+namespace HomeWorkFirst.Myclasses
+{
+    public class MathCalculation
+    {
+       public int numbers;
+
+       public MathCalculation()
+       {
+        System.Console.WriteLine("введите  любые числа через запятую");
+       }
+
+       
+    }
+}
