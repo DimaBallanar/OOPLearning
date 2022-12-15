9c97b5b6d85ec8f4456e50d4b2187fb6d5f4cc22
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-15 20:27:42 +0300

    OOPDaily

---
+++ work1412/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace OOPTask2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone();
+            Phone phone2 = new Phone();
+            phone2.Number = "+375255218579";
+            phone2.Model = "Iphone";
+            phone2.Weigth = "1кг";
+            Phone phone3 = new Phone()
+            {
+                Number = "+375255435579",
+                Model = "Honor",
+                Weigth = "4кг"
+            };
+            System.Console.WriteLine(phone1);
+            System.Console.WriteLine(phone2);
+            System.Console.WriteLine(phone3);
+
+        }
+    }
+}
