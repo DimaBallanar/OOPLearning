832c9220e04b2e3ecda0e44c91c2296c2ebfcad4
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-16 02:05:19 +0300

    work work work

--- FirstHomeWork/Myclasses/Fibonacci.cs
+++ FirstHomeWork/Myclasses/Fibonacci.cs
@@ -4,5 +4,22 @@ namespace HomeWorkFirst.Myclasses;
 
 public class Fibonacci
 {
-    
+    static void Task1()
+        {
+            System.Console.WriteLine("введите номер элемента с ряда Фиббоначчи");
+            int cost = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine(FibNumber(cost));
+        }
+        static int FibNumber(int cost)
+        {
+            if (cost == 1)
+            {
+                return 0;
+            }
+            if (cost == 2 || cost == 3)
+            {
+                return 1;
+            }
+            return FibNumber(cost - 1) + FibNumber(cost - 2);
+        }
 }
