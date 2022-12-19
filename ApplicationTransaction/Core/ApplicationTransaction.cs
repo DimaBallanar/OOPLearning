47d3da87a4277486eb0b2a4323da9e12aeffdec1
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-19 22:58:19 +0300

    daily practic

--- ApplicationTransaction/Core/ApplicationTransaction.cs
+++ ApplicationTransaction/Core/ApplicationTransaction.cs
@@ -11,9 +11,9 @@ public class ApplicationTransaction
     {
         Users = new User[4]{
         new User(1,"test1","test2","1111@gmail.com"),
-        new User(2, "test2", "test2", "1111@gmail.com"),
-        new User(3, "test3", "test2", "1111@gmail.com"),
-        new User(4, "test4", "test2", "1111@gmail.com"),
+        new User(2, "test2", "test2", "2111@gmail.com"),
+        new User(3, "test3", "test2", "3111@gmail.com"),
+        new User(4, "test4", "test2", "4111@gmail.com"),
     };
     }
 
@@ -23,6 +23,9 @@ public class ApplicationTransaction
         if (autorization.Login(Users, out User? user))
         {
             System.Console.WriteLine(user);
+            WorkflowTransaction workflowTransactions = new WorkflowTransaction();
+            workflowTransactions.Show(user);
+
         }
 
     }
--- ApplicationTransaction/Service/Autorization.cs
+++ ApplicationTransaction/Service/Autorization.cs
@@ -38,3 +38,4 @@ public class Autorization
 }
 
 
+//проверка на ввод мыла
