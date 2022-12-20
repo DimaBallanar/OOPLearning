0b7c71388ffe26b6fdaf9c00abe7e0928f963f88
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-20 20:53:46 +0300

    Registration.cs

---
+++ ApplicationTransaction/Service/Registration.cs
@@ -0,0 +1,47 @@
+
+namespace Registration.Service;
+
+public class Registration
+{
+    private readonly int[] IdUserMass;
+    public int IdUser { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+
+
+    public Registration(int id, string name, string password, string email)
+    {
+        IdUser = id;
+        Name = name;
+        Password = password;
+        Email = email;
+    }
+    static int[] MassId()
+    {
+        int[] massiv = new int[100];
+
+        for (int i = 0; i < massiv.Length; i++)
+        {
+            massiv[i] += i;
+        }
+        return massiv;
+    }
+    static int GetIdUser(string name,ref int[] massId)
+    {
+        int id;
+        if (!string.IsNullOrEmpty(name))
+        {
+
+        }
+
+        int[] massiv = new int[100];
+
+        for (int i = 0; i < massiv.Length; i++)
+        {
+            massiv[i] += i;
+        }
+
+        return id;
+    }
+}
