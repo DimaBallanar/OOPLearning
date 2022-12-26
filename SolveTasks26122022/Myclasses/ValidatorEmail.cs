a724fb38fe793028fdd3b86dc503d684d38e6e99
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-26 22:37:25 +0300

    daily

---
+++ SolveTasks26122022/Myclasses/ValidatorEmail.cs
@@ -0,0 +1,37 @@
+
+
+namespace ValidatorE
+{
+
+    public class ValidatorEmail
+    {
+
+        public bool CheckEmail(string text)
+        {
+            int countSobak = 0;
+            foreach (char a in text)
+            {
+                if (a == '@' && countSobak < 2)
+                {
+                    countSobak += 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string[] massiv = text.Split('@');
+            string myCheck = massiv[1];
+            string[] result = myCheck.Split('.');
+            if (result.Length == 2 && result[1].Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
