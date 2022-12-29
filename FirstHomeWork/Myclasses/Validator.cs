269363677c9b58574d305b77035315401401092a
Dima Ballanar <D.Ballanar@gmail.com>
2022-12-29 22:32:13 +0300

    вносим правки

--- FirstHomeWork/Myclasses/Validator.cs
+++ FirstHomeWork/Myclasses/Validator.cs
@@ -12,9 +12,31 @@ public class Validator
 
     public string email;
 
-    public bool IsEmail()
-    {
-        
-    }
+   public bool CheckEmail(string text)
+        {
+            int countSobak = 0;
+            foreach (char a in text)
+            {
+                if (a == '@' && countSobak < 2)
+                {
+                    countSobak += 1;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string[] massiv = text.Split('@');
+            string myCheck = massiv[1];
+            string[] result = myCheck.Split('.');
+            if (result.Length == 2 && result[1].Length > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
 }
