42aaccb51336a866c937a5a8a7474ac10204d9f3
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-01 20:25:59 +0300

    исправление ошибок

--- FirstHomeWork/Program.cs
+++ FirstHomeWork/Program.cs
@@ -6,14 +6,14 @@ namespace HomeWorkFirst //
     {
         static void Main(string[] args)
         {
-            // Singer make = new Singer();
-            // make.GetАutograph();
-            // WordString use = new WordString();
-            // System.Console.WriteLine(use.ReverseString());
-            // System.Console.WriteLine(use.UpperFirst());
-            // System.Console.WriteLine(use.UpperEvery());
-            // MathCalculation collect = new MathCalculation();
-            // System.Console.WriteLine(collect.CheckPolar());
+            Singer make = new Singer();
+            make.GetАutograph();
+            WordString use = new WordString();
+            System.Console.WriteLine(use.ReverseString());
+            System.Console.WriteLine(use.UpperFirst());
+            System.Console.WriteLine(use.UpperEvery());
+            MathCalculation collect = new MathCalculation();
+            System.Console.WriteLine(collect.CheckPolar());
             Validator validator=new Validator(Console.ReadLine());
             System.Console.WriteLine(validator.CheckEmail());
             System.Console.WriteLine(validator.IsPhone());
