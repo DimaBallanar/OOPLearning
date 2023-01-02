15fdc0f015c42309b905ab95ad8bb3bd008ffbe0
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-02 21:54:53 +0300

    homeWork Update

--- SolveTasks26122022/Program.cs
+++ SolveTasks26122022/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Server;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ServerGetAll server=new ServerGetAll();
+            server.Controller();
         }
     }
 }
