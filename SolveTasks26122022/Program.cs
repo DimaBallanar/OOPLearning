a239ba32c6be11ae49950d6c3053d3041757db46
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-04 13:11:10 +0300

    update work

--- SolveTasks26122022/Program.cs
+++ SolveTasks26122022/Program.cs
@@ -2,6 +2,7 @@
 using Server;
 using ServerBy;
 using ServerPo;
+using ServerP;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -11,15 +12,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             // ServerGetAll server=new ServerGetAll();
             // server.Controller();
-            // System.Console.Write("укажите id пользователя");
+            System.Console.Write("укажите id пользователя: ");
+            int id = Convert.ToInt32(Console.ReadLine());
             // ServerByld serv=new ServerByld();
             // serv.Controller(3);
             System.Console.Write("Введите имя: ");
             string name = Convert.ToString(Console.ReadLine());
             System.Console.Write("Введите возраст: ");
             int age = Convert.ToInt32(Console.ReadLine());
-            ServerPost collect = new ServerPost();
-            collect.Controller(name, age);
+            // ServerPost collect = new ServerPost();
+            // collect.Controller(name, age);
+            ServerPut zamena = new ServerPut();
+            zamena.Controller(id, name, age);
 
 
 
