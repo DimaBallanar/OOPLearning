b34e237569a60ae9dac9bfb48a27f5921af29b16
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-04 11:55:49 +0300

    добавление пользователя

--- SolveTasks26122022/Program.cs
+++ SolveTasks26122022/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Server;
 using ServerBy;
+using ServerPo;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -8,10 +9,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            ServerGetAll server=new ServerGetAll();
-            server.Controller();
+            // ServerGetAll server=new ServerGetAll();
+            // server.Controller();
             // ServerByld serv=new ServerByld();
             // serv.Controller(3);
+            System.Console.Write("Введите имя: ");
+            string? name=Convert.ToString(Console.ReadLine());
+            System.Console.Write("Введите возраст: ");
+            int age=Convert.ToInt32(Console.ReadLine());
+            ServerPost collect=new ServerPost();
+            collect.Controller(name,age);
+
+
         }
     }
 }
