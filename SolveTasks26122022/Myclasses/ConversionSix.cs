5cf51a354f3109bfaf833166752fe9eeff468ef6
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-06 17:04:10 +0300

    update

--- SolveTasks26122022/Myclasses/ConversionSix.cs
+++ SolveTasks26122022/Myclasses/ConversionSix.cs
@@ -46,4 +46,5 @@ public class ConversionSix
         }
         return result;
     }
+    
 }
---
+++ SolveTasks26122022/Myclasses/Sort.cs
@@ -0,0 +1,13 @@
+// Реализуйте класс Sort, сдержащий метод SortData, сортирующий все числа
+// статичного массива сортировкой пузырьком
+
+namespace SortPuzir;
+
+public class Sort
+{
+
+    public int [] SortBubble(int[] array)
+    {
+        
+    }
+}
