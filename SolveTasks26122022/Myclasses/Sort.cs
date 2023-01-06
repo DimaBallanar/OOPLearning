6a688909ff42a2898ca30540a13d452e4be5d6ea
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-06 17:34:24 +0300

    finish

--- SolveTasks26122022/Myclasses/Sort.cs
+++ SolveTasks26122022/Myclasses/Sort.cs
@@ -6,8 +6,19 @@ namespace SortPuzir;
 public class Sort
 {
 
-    public int [] SortBubble(int[] array)
+    public void SortData(int[] array)
     {
-        
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+                if (array[j] > array[j + 1])
+                {
+                    int timeNum = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = timeNum;
+                }
+        }
+        System.Console.WriteLine(string.Join(",", array));
+
     }
 }
