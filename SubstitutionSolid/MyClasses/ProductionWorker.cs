eeb1e7194107551d67fd11cc5b6f1999f6000656
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-07 16:02:13 +0300

    update class

--- SubstitutionSolid/MyClasses/ProductionWorker.cs
+++ SubstitutionSolid/MyClasses/ProductionWorker.cs
@@ -1,23 +1,30 @@
 using Myclasses.Employee;
 
-public class ProductionWorker:Employee
+public class ProductionWorker : Employee
 {
-    public int WorkPeriod{get;set;}
-    public double MoneyPerHour{get;set;}
+    public int WorkPeriod { get; set; }
+    public double MoneyPerHour { get; set; }
 
-    public ProductionWorker(string name,int number,int workperiod,double money):base(name,number)
+    public ProductionWorker(string name, int number, int workperiod, double money) : base(name, number)
     {
-        WorkPeriod=workperiod;
-        MoneyPerHour=money;
+        WorkPeriod = workperiod;
+        MoneyPerHour = money;
     }
     public int Period(string text)
     {
         System.Console.WriteLine("введите смену в которую вы работаете? дневная/вечерняя/ночная");
-        
-        if(text=="дневная"){return 1;}
-        
-        if(text=="вечерняя"){return 2;}
-        else {return 3;}
+
+        if (text == "дневная") { return 1; }
+
+        if (text == "вечерняя") { return 2; }
+        else { return 3; }
+    }
+    public double CashMoney(int workperiod)
+    {
+        if (workperiod == 1) { return 8.2; }
+        if (workperiod == 2) { return 10; }
+        else { return 20; }
+
     }
 
 }
