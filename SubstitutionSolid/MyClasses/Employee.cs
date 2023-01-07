837e209dcccc15652b0a9c9c5aa1087e6b12db1a
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-07 15:47:57 +0300

    update classes

--- SubstitutionSolid/MyClasses/Employee.cs
+++ SubstitutionSolid/MyClasses/Employee.cs
@@ -4,5 +4,15 @@ public class Employee
 {
     public string Name{get;set;}
     public int Number{get;set;}
-    
+
+    public Employee(string name)
+    {
+        Name=name;
+        
+    }
+
+    public Employee(string name,int number):this(name)
+    {
+        Number=number;
+    }
 }
--- SubstitutionSolid/MyClasses/ProductionWorker.cs
+++ SubstitutionSolid/MyClasses/ProductionWorker.cs
@@ -4,5 +4,10 @@ public class ProductionWorker:Employee
 {
     public int WorkPeriod{get;set;}
     public double MoneyPerHour{get;set;}
-    
+
+    public ProductionWorker(string name,int number,int workperiod,double money):base(name,number)
+    {
+        WorkPeriod=workperiod;
+        MoneyPerHour=money;
+    }
 }
