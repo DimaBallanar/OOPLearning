a771f60808a4cbc2119fd2992ab2521e84d76a7c
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-09 18:18:36 +0300

    update singer

---
+++ SubstitutionSolid/MyClasses/Singer.cs
@@ -0,0 +1,43 @@
+using Myclasses.Worker;
+
+// Класс Singer наследует Name, Surname, также состоит из свойств Years, Birth, Style и метода
+// GetMoreInfo(), который выводит информацию о певце: Years, Birth, Style.
+// Необходимо вызвать метод GetMoreInfo
+
+public class Singer : Worker
+{
+    public int Years { get; set; }
+    public DateOnly Birth { get; set; }
+    public string Style { get; set; }
+
+    public Singer()
+    {
+
+    }
+
+    public Singer(string name, string surname, int years, DateOnly birth, string style) : base(name, surname)
+    {
+        Birth = GetBirth();
+        Years = GetYears();
+
+    }
+
+    public DateOnly GetBirth()
+    {
+        System.Console.WriteLine("введите год рождения");
+        int x = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("введите месяц рождения");
+        int y = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("введите день рождения");
+        int z = Convert.ToInt32(Console.ReadLine());
+        DateOnly birth = new DateOnly(x, y, z);
+        return birth;
+
+    }
+    public int GetYears()
+    {
+        DateTime now = DateTime.Now;
+        int years = now.Year - Birth.Year;
+        return years;
+    }
+}
