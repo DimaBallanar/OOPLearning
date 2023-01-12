f20832914472899d26efe7d35a1745e8ee38b292
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-12 13:43:57 +0300

    update

--- Solid0501/Incapsulation/Animals.cs
+++ Solid0501/Incapsulation/Animals.cs
@@ -1,7 +1,26 @@
 namespace AllAnimals;
 
-public class AllAnimals
+public class Animals
 {
-    public int Food{get;set;}
-    
+    public string Name { get; set; }
+    public string TypeOfAnimal { get; set; }  // имя
+    public string Type { get; set; }   //тип пищи
+    public int CountFood { get; set; }  //количество пищи
+
+
+    public Animals(string name, string typeOfAnimal, int countFood)
+    {
+        Name = name;
+    }
+
+    public virtual string FoodCounter()
+    {
+        System.Console.Write("сколько ест за прием?:");
+        double foodPerTime = Convert.ToDouble(Console.ReadLine());
+
+
+    }
+
+
+
 }
--- Solid0501/Incapsulation/Predator.cs
+++ Solid0501/Incapsulation/Predator.cs
@@ -1,6 +1,17 @@
 namespace AllAnimals;
 
-public class Predator:AllAnimals
+public class Predator : Animals
 {
-    
+    public string Type
+    {
+        get { return "хищник"; }
+    }
+
+    public Predator(string name, string type) : base(name)
+    {
+        
+    }
+
+
+
 }
