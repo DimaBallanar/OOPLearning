1825c32a9e925ff0a9121f087157b9b70fb3564f
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-12 18:22:32 +0300

    update

--- Solid0501/Incapsulation/Buyer.cs
+++ Solid0501/Incapsulation/Buyer.cs
@@ -14,8 +14,31 @@ public class BuyerData
     private string Patronymic;  //отчество
 
     private string Addres;
-    private int CreditCard;
+    private ulong CreditCard;
     private int BankAccount;
+
+    public BuyerData(string firstName, string secondname, string patronymic, string adress, ulong creditCard, int bankAccount)
+    {
+        FirstName = firstName;
+        SecondName = secondname;
+        Patronymic = patronymic;
+        Addres = adress;
+        CreditCard = creditCard;
+        BankAccount = bankAccount;
+    }
+    public BuyerData()
+    {
+        BuyerData[] array = new BuyerData[4];
+        {
+            array[0] = new BuyerData("дима", "Пупкин", "владимирович", "игуменский тракт 47 квартира 75", 000000001, 1223);
+            array[1] = new BuyerData("вася", "Грач", "Степанович", "игуменский тракт 44 квартира 75", 000666000, 1223);
+            array[2] = new BuyerData("петя", "Топор", "Васильевич", "игуменский тракт 98 квартира 75", 100000000, 1223);
+            array[3] = new BuyerData("коля", "Корыто", "Георгиевич", "игуменский тракт 4 квартира 75", 123400001, 1223);
+        };
+    }
     
+
+
+
 
 }
