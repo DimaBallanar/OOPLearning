494acf22ee46f0a8d0e9cc6e6ae82f2a39aa1a26
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-18 21:11:57 +0300

    update task2

---
+++ HomeWorkAbstract/MyClasses/User.cs
@@ -0,0 +1,12 @@
+namespace Myclasses;
+
+public abstract class User
+{
+    public int UserId { get; set; }
+    public string Name { get; set; } = "Noname";
+    public string SurName { get; set; } = "NoSurname";
+    public string Pwd { get; set; } = "";
+
+
+
+}
