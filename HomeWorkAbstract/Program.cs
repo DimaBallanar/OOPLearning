e25702ba66208ac394b50484b8ada7dc7026d1dc
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-18 21:24:12 +0300

    update

--- HomeWorkAbstract/Program.cs
+++ HomeWorkAbstract/Program.cs
@@ -11,6 +11,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
             System.Console.WriteLine(tro.IsValidEmail("D.Ballanar@gmail.com"));
             System.Console.WriteLine(tro.IsValidPwd("1234123", "1234123"));
             System.Console.WriteLine(tro.IsValidId("144"));
+            Server check=new Server(12,"Dima","Balla","123WEsd");
+            check.GetUser();
 
         }
     }
