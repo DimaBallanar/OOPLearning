5e9ba510f3c85f6402f4a6fad843a34853381117
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-18 21:17:15 +0300

    update

--- HomeWorkAbstract/MyClasses/Server.cs
+++ HomeWorkAbstract/MyClasses/Server.cs
@@ -1,6 +1,12 @@
 namespace Myclasses;
 
-public class Server:User
+public class Server : User
 {
-    
+    public Server(int userId, string name, string surName, string pwd)
+    {
+        UserId = userId;
+        Name = name;
+        SurName = surName;
+        Pwd = pwd;
+    }
 }
