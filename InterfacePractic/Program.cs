e4e45b4726a47974a0d87a5dec930ecc5023f7f4
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-19 21:08:52 +0300

    update

--- InterfacePractic/Program.cs
+++ InterfacePractic/Program.cs
@@ -15,10 +15,25 @@ namespace MyApp // Note: actual namespace depends on the project name.
             // System.Console.WriteLine((int)WalletTypes.Crypto);
             // System.Console.WriteLine((int)WalletTypes.Leather);
             // System.Console.WriteLine((WalletTypes)1);
-            IPayments wallet = new LeatherWallet(10);
-            wallet.PushMoney(1233);
-            Shop shop = new Shop();
-            shop.PayCash(wallet);
+            // IPayments wallet = new LeatherWallet(10);
+            // wallet.PushMoney(1233);
+            // Shop shop = new Shop();
+            // shop.PayCash(wallet);
+
+            // лами while, foreach
+            string[] array = { "2131231", "fff" };
+            int number = 0;
+            while (number < array.Length)
+            {
+                System.Console.WriteLine(array[number++]);
+               
+            }
+
+            foreach(string str in array)
+            {
+                System.Console.WriteLine(str);
+            }
+
 
         }
     }
