87a390986ec5dd0a36ab1f640a4b62118f88c955
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-26 19:14:05 +0300

    update

--- Solid0501/MyClasses/GenericHome.cs
+++ Solid0501/MyClasses/GenericHome.cs
@@ -11,19 +11,33 @@ public class GenericHome<T>
 {
     private T[] Items;
 
-    public GenericHome()
+    public GenericHome(T item)
     {
-        T[] Items = new T[] { };
+        T[] Items = new T[1] { default };
     }
 
-    public void  AddItem(T item)
+    public void AddItem(T item)
     {
-        T[] timearray = new T[Items.Length + 1] { };
+        T[] timearray = new T[++Items.Length] { };
         Array.Copy(Items, timearray, Items.Length);
         timearray[timearray.Length - 1] = item;
         Items = timearray;
     }
-    
+
+    public bool FindIndex(T item, out int index)
+    {
+        index = -1;
+        for (int i = 0; i < Items.Length; i++)
+        {
+            if (item.Equals(Items[i]))
+            {
+                index = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 
 }
