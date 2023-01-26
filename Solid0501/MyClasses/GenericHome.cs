90ce35f223d4191cf0a2ea06f6db9107a24b95c0
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-26 18:56:11 +0300

    update

--- Solid0501/MyClasses/GenericHome.cs
+++ Solid0501/MyClasses/GenericHome.cs
@@ -16,13 +16,14 @@ public class GenericHome<T>
         T[] Items = new T[] { };
     }
 
-    public T AddItem(T item)
+    public void  AddItem(T item)
     {
         T[] timearray = new T[Items.Length + 1] { };
         Array.Copy(Items, timearray, Items.Length);
-
-
+        timearray[timearray.Length - 1] = item;
+        Items = timearray;
     }
+    
 
 
 }
