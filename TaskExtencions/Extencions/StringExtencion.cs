545c05b0cf2b47ebc7fee092ea4d0064c56489db
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-30 22:21:41 +0300

    update

---
+++ TaskExtencions/Extencions/StringExtencion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Написать метод раширения, который преобразует тип string в тип int с проверкой строки на 
+//наличие только чисел
+
+namespace TaskExtencions.Extencions
+{
+    public static class StringExtencion
+    {
+        
+        public static int ToInt(this string str)
+        {
+            string result = "";
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += c;
+                }
+
+            }
+            return string.IsNullOrEmpty(result) ? 0 : int.Parse(result);
+
+        }
+    }
+}
---
+++ TaskExtencions/Program.cs
@@ -0,0 +1,15 @@
+﻿using TaskExtencions.Extencions;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "tr2324e2ddf";
+            int result = text.ToInt();
+            Console.WriteLine(result);
+
+        }
+    }
+}
