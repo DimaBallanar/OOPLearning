36d681cc74c7fdedc7d017aa07f37375be70c355
Dima Ballanar <D.Ballanar@gmail.com>
2023-01-30 14:53:00 +0300

    update

---
+++ controlPoint/MyInterface/Bus.cs
@@ -0,0 +1,22 @@
+namespace MyInterface;
+
+public class Bus:Transport, IDrive
+{
+public Bus(string name,int number):base(name="автобус",number)
+{
+	
+}
+    public void DriveAway()
+    {
+    Console.WriteLine($"автобус {Number} уехал");
+    }
+    void DriveBack()
+    {
+    Console.WriteLine("автобус {Number}приехал");
+    }
+
+	public override Move()
+	{
+	Console.WriteLine("автобус {Number} едет");
+	}
+}
--- controlPoint/Program.cs
+++ controlPoint/Program.cs
@@ -13,28 +13,7 @@
 
 
 
-namespace MyInterface;
 
-public class Bus:Transport, IDrive
-{
-public Bus(string name,int number):base(name="автобус",number)
-{
-	
-}
-    public void DriveAway()
-    {
-    Console.WriteLine($"автобус {Number} уехал");
-    }
-    void DriveBack()
-    {
-    Console.WriteLine("автобус {Number}приехал");
-    }
-
-	public override Move()
-	{
-	Console.WriteLine("автобус {Number} едет");
-	}
-}
 
 namespace MyInterface;
 
