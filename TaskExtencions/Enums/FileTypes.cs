0122f4d6a45ef905c31a89269ececa23d86ccc85
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-01 22:27:54 +0300

    update

---
+++ TaskExtencions/Enums/FileTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExtencions.Enums
+{
+    public enum FileTypes
+    {
+        pdf=1,
+        txt=2,
+        doc=3,
+        html=4,
+        svg=5
+    }
+}
---
+++ TaskExtencions/Extencions/FileExtencion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskExtencions.Enums;
+
+namespace TaskExtencions.Extencions
+{
+    public static class FileExtencion
+    {
+        public static string FileTypeMessage(this FileTypes xxx)
+        {
+            if(Enum.IsDefined(xxx))
+            {
+                return $"Выбран файл с расширением .{xxx}";
+            }
+            throw new Exception("нет в наличии");
+        }
+    }
+}
