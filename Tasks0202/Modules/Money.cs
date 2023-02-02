0e79005f8d274b1e9d3aa50f9cce12daa6438988
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-02 22:03:39 +0300

    update

---
+++ Tasks0202/Modules/Money.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Tasks0202.Modules
+{
+    public class Money
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        [JsonPropertyName("d")]
+        [JsonIgnore(Condition =JsonIgnoreCondition.WhenWritingDefault)]
+        public DateTime? LastUpdate { get; set; }
+        [JsonIgnore]
+        public string Text => $"{Code}+{Name}";
+
+        public Money()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Code}   {Name}  {Symbol}";
+        }
+    }
+}
---
+++ Tasks0202/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Tasks0202.Modules;
+using System.Text.Json;
+
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money dollar = new Money() { Code = 177, Name = "Dollar", Symbol = "$" ,LastUpdate=DateTime.Now};
+            Money belRub = new Money() { Code = 667, Name = "BelRuble", Symbol = "Br" };
+
+            Console.WriteLine(dollar.ToString());
+            Console.WriteLine(belRub.ToString());
+
+            string jsonDollar = JsonSerializer.Serialize(dollar);
+            Console.WriteLine(jsonDollar);
+            string jsonBelRub = JsonSerializer.Serialize(belRub);
+            Console.WriteLine(jsonBelRub);
+            Money newDollar = JsonSerializer.Deserialize<Money>(jsonDollar);
+            Console.WriteLine(newDollar);
+            Money newByn = JsonSerializer.Deserialize<Money>(jsonBelRub);
+            Console.WriteLine(newByn);
+            Console.WriteLine("----Custome----");
+            var serializeoptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+            string json = "{\"code\":177,\"Name\":\"dollar\",\"Symbol\":\"$\"}";
+            try
+            {
+                Money test = System.Text.Json.JsonSerializer.Deserialize<Money>(json,serializeoptions);
+                Console.WriteLine(test);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+
+        }
+    }
+}
