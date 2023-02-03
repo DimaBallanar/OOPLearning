c33f577860c973f346a0a0cb43bc9456a267478e
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-03 20:09:30 +0300

    update

--- TaskExtencions/Extencions/FileNameExt.cs
+++ TaskExtencions/Extencions/FileNameExt.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using TaskExtencions.Enums;
@@ -51,6 +52,34 @@ namespace TaskExtencions.Extencions
                 return result;
             }
             throw new Exception("не удалось");
+        }
+        /*
+                7. Написать метод расширения проверки пароля, который проверяет, чтобы строка была не
+        менее 8 элементов, буквы в большом региристре, буквы в маленьком региристре, цифра, и
+        один спецсимвол из списка !@#$%^&*|, другие спецсимволы не должны встречаться в
+        пароле.Для каждого исключения бросать свою ошибку со своим кодом(создать
+        пользовательскую ошибку и написать перечисление ошибок)
+        */
+
+        public static bool PassCheck(this string userPass)
+        {
+            string pattern = @"!@#$%^&*|";
+            if (!string.IsNullOrEmpty(userPass))
+            {
+                int countB = 0;
+                int countNum = 0;
+                int countSymb = 0;
+                foreach (char c in userPass)
+                {
+                    if (char.IsUpper(c)) countB++;
+                    if (char.IsDigit(c)) countNum++;
+                    if (Regex.IsMatch(userPass, pattern)) countSymb++;
+                }
+                if (countB < 1) throw new Exception("нет прописной буквы");
+                if (countNum < 1) throw new Exception("нет цифр");
+                if (countSymb != 1) throw new Exception("только 1 спецсимвол");
+            }
+            return true;
         }
 
     }
