048232d1bf1624748d76c2670235fa63c13a808e
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-09 02:13:30 +0300

    update

--- controlPoint/MyInterface/TransportSystemCity.cs
+++ controlPoint/MyInterface/TransportSystemCity.cs
@@ -3,9 +3,9 @@ namespace MyInterface;
 public class TransportSystemCity
 {
     private Transport[] Transports;
-    private int CountShip;
-    private int CountBus;
     private int CountPlane;
+    private int CountBus;
+    private int CountShip;
 
     public TransportSystemCity()
     {
@@ -14,13 +14,12 @@ public class TransportSystemCity
 
     public void SetInAirport(Plane plane)
     {
-        if (CountPlane == 11) throw new Exception("нет места в массиве для самолёта");
-
+        if (CountPlane == 11) throw new Exception("нет места в массиве");
         for (int i = 0; i < Transports.Length; i++)
         {
             if (Transports[i] == null)
             {
-                plane.SitGround();
+                plane.Posadka();
                 Transports[i] = plane;
                 CountPlane++;
                 return;
@@ -30,12 +29,12 @@ public class TransportSystemCity
 
     public void SetInBusStation(Bus bus)
     {
-        if (CountBus == 11) throw new Exception("нет места в массиве для автобуса");
-        for (int i = 0; i < Transports.Length; i++)
+        if (CountBus == 11) throw new Exception("нет места в массиве");
+        for (int i = 12; i < Transports.Length; i++)
         {
             if (Transports[i] == null)
             {
-                bus.DriveBack();
+                bus.Priehal();
                 Transports[i] = bus;
                 CountBus++;
                 return;
@@ -45,12 +44,12 @@ public class TransportSystemCity
 
     public void SetInPort(Ship ship)
     {
-        if (CountShip == 11) throw new Exception("нет места в массиве для корабля");
-        for (int i = 0; i < Transports.Length; i++)
+        if (CountShip == 11) throw new Exception("нет места в массиве");
+        for (int i = 23; i < Transports.Length; i++)
         {
             if (Transports[i] == null)
             {
-                ship.SwimHere();
+                ship.Priplil();
                 Transports[i] = ship;
                 CountShip++;
                 return;
@@ -58,59 +57,185 @@ public class TransportSystemCity
         }
     }
 
-    public Plane GetFromAirPort(int number)
+
+    public Plane GetFromAirport(int number)
     {
         for (int i = 0; i < Transports.Length; i++)
         {
             var transport = Transports[i];
-            if (transport == null) continue;
-            if (transport is Plane plane && transport.Number == number)
+            if (transport == null)
+            {
+                continue;
+            }
+            if (transport is Plane plane && transport.NumberOfTransport == number)
             {
                 Transports[i] = null;
-                plane.FlyAway();
+                plane.Vzlet();
                 CountPlane--;
                 return plane;
             }
         }
-        throw new Exception("нет элемента в массиве");
-
+        throw new Exception("нет самолета на базе");
     }
+
     public Bus GetFromBusStation(int number)
     {
-        for (int i = 0; i < Transports.Length; i++)
+        for (int i = 12; i < Transports.Length; i++)
         {
             var transport = Transports[i];
-            if (transport == null) continue;
-            if (transport is Bus bus && transport.Number == number)
+            if (transport == null)
             {
-                Transports[i] = default;
-                bus.DriveAway();
+                continue;
+            }
+            if (transport is Bus bus && transport.NumberOfTransport == number)
+            {
+                Transports[i] = null;
+                bus.Poehal();
                 CountBus--;
                 return bus;
             }
         }
-        throw new Exception("нет элемента в массиве");
+        throw new Exception("нет автобуса на базе");
     }
-
 
     public Ship GetFromPort(int number)
     {
-        for (int i = 0; i < Transports.Length; i++)
+        for (int i = 23; i < Transports.Length; i++)
         {
             var transport = Transports[i];
-            if (transport == null) continue;
-            if (transport is Ship ship && transport.Number == number)
+            if (transport == null)
             {
-                Transports[i] = default;
-                ship.SwimAway();
+                continue;
+            }
+            if (transport is Ship ship && transport.NumberOfTransport == number)
+            {
+                Transports[i] = null;
+                ship.Poplil();
                 CountShip--;
                 return ship;
             }
+        }
+        throw new Exception("нет корабля в порту");
+    }
 
+    public (bool, string) BuyPlaneTicket(int number, int countSeats)
+    {
+        bool aprove = true;
+        string result = "";
+        int check = 0;
+        for (int i = 0; i < Transports.Length; i++)
+        {
+            var transport = Transports[i];
+            if (transport == null)
+            {
+                continue;
+            }
 
+            if (transport.NumberOfTransport == number && transport is Plane plane)
+            {
+                check++;
+
+                if (transport.CountSeats >= countSeats)
+                {
+                    aprove = true;
+                    result = $"Вы купили {countSeats}";
+                    transport.CountSeats = transport.CountSeats - countSeats;
+                    return (aprove, result);
+                }
+                else
+                {
+                    aprove = false;
+                    result = $"У транспорного средства осталось {transport.CountSeats} свободных мест";
+                    
+                }
+
+                
+            }
+
+            continue;
 
         }
-        throw new Exception("нет элемента в массиве");
+        if (check == 0) throw new Exception("нет самолета на базе");
+        return (aprove, result);
+    }
+
+    public (bool, string) BuyBusTicket(int number, int countSeats)
+    {
+        bool aprove = true;
+        string result = "";
+        int check = 0;
+        for (int i = 12; i < Transports.Length; i++)
+        {
+
+            var transport = Transports[i];
+            if (transport == null)
+            {
+                continue;
+            }
+
+            if (transport.NumberOfTransport == number && transport is Bus bus)
+            {
+                check++;
+                if (transport.CountSeats >= countSeats)
+                {
+                    aprove = true;
+                    result = $"Вы купили {countSeats}";
+                    transport.CountSeats = transport.CountSeats - countSeats;
+                    return (aprove, result);
+                }
+                else
+                {
+                    aprove = false;
+                    result = $"У транспорного средства осталось {transport.CountSeats} свободных мест";
+                    return (aprove, result);
+                }
+
+                
+            }
+
+        }
+        if (check == 0) throw new Exception("нет автобуса на базе");
+        return (aprove, result);
+    }
+
+
+    public (bool, string) BuyShipTicket(int number, int countSeats)
+    {
+        bool aprove = true;
+        string result = "";
+        int check = 0;
+        for (int i = 23; i < Transports.Length; i++)
+        {
+            var transport = Transports[i];
+            if (transport == null)
+            {
+                continue;
+            }
+
+            if (transport.NumberOfTransport == number && transport is Ship ship)
+            {
+                check++;
+                if (transport.CountSeats >= countSeats)
+                {
+                    aprove = true;
+                    result = $"Вы купили {countSeats}";
+                    transport.CountSeats = transport.CountSeats - countSeats;
+                    return (aprove, result);
+                }
+                else
+                {
+                    aprove = false;
+                    result = $"У транспорного средства осталось {transport.CountSeats} свободных мест";
+                    return (aprove, result);
+                }
+
+                
+            }
+
+        }
+        if (check == 0) throw new Exception("нет корабля в порту");
+        return (aprove, result);
     }
 
 }
+
--- controlPoint/Program.cs
+++ controlPoint/Program.cs
@@ -7,8 +7,13 @@ namespace MyApp
         static void Main(string[] args)
         {
             TransportSystemCity transportsystem=new TransportSystemCity();
-            transportsystem.SetInAirport(new Plane("airbus1", 776));
-            transportsystem.SetInAirport(new Plane("airbus1", 777));
+            transportsystem.SetInAirport(new Plane("airbus1", 776,11));
+            transportsystem.SetInAirport(new Plane("airbus1", 777,11));
+            transportsystem.GetFromAirport(776);            
+            Console.WriteLine(transportsystem.BuyPlaneTicket(779, 8));       
+            Console.WriteLine(transportsystem.BuyPlaneTicket(777, 3));     
+            Console.WriteLine(transportsystem.BuyPlaneTicket(777, 2));
+
         }
     }
 }
