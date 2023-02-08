a604f764d05a35859217aed4606222552093ce66
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-08 23:13:55 +0300

    update

--- controlPoint/MyInterface/Bus.cs
+++ controlPoint/MyInterface/Bus.cs
@@ -4,9 +4,7 @@ public class Bus : Transport, IDrive
 {
 
     public Bus(string name, int number) : base(name = "автобус", number)
-    {
-
-    }
+    { }
     public override void Move()
     {
         Console.WriteLine($"автобус {Name} {Number} едет");
