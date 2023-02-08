c4ccbf4cb0d0d2c8efa6f61dfa59126f18b26f37
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-08 23:14:39 +0300

    update

--- controlPoint/MyInterface/Plane.cs
+++ controlPoint/MyInterface/Plane.cs
@@ -1,24 +1,22 @@
 namespace MyInterface;
 
-public class Plane:Transport, IFly
+public class Plane : Transport, IFly
 {
-public Plane(string name,int number):base(name="Plane",number)
-{
-	
-}
+    public Plane(string name, int number) : base(name = "Plane", number)
+    { }
     public void FlyAway()
     {
-    Console.WriteLine($"Plane {Name} {Number} улетел");
+        Console.WriteLine($"Plane {Name} {Number} улетел");
     }
     public void SitGround()
     {
-    Console.WriteLine($"Plane  {Name} {Number} прилетел");
+        Console.WriteLine($"Plane  {Name} {Number} прилетел");
     }
 
-	public override void Move()
-	{
-	Console.WriteLine($"Plane {Name} {Number} летит");
-	}
+    public override void Move()
+    {
+        Console.WriteLine($"Plane {Name} {Number} летит");
+    }
 
-  
+
 }
