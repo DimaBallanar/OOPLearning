ca4ff260732e607ae0064f5e08f6611beb2d2eb1
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-16 00:13:27 +0300

    update

---
+++ Work1302/Task2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Напишите программу, содержащую статический метод с тремя действительными (тип
+//double) аргументами.Результатом статического метода является ссылка на метод,
+//вычисляющий квадратичный трехчлен с коэффициентами, определяемыми аргументами
+//статического метода. Другими словами, если статический метод вызывается с числовыми
+//аргументами a, b и с, то результатом является ссылка на метод, который для аргумента x
+//типа double результатом вычисляет значение выражения a*x*x+b*x+c.
+
+
+namespace Work1302
+{
+    delegate double MethodCifr(double num);
+    public class Task2
+    {
+        public static double Methodica(double a,double b, double c)
+        {
+
+        }
+
+    }
+}
