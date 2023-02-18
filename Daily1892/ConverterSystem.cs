b4e87c639718eaed1b9d0e9819a88df8e09e79c4
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-18 12:21:29 +0300

    update

---
+++ Daily1892/ConverterSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daily1892
+{
+    public static class ConverterSystem
+    {
+        public static string ConvertBinary(this int value)
+        {
+            List<object> result = new List<object>();            
+            object[] array = { 0, 1 };
+            int binary = array.Length;
+            while (value > 0)
+            {
+                result.Add(array[value % binary]);
+                value /= binary;
+            }
+            result.Reverse();
+            return string.Join("", result);
+        }
+    }
+}
