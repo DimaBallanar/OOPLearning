5b1eb791644cc2cdc5aac3cf6fdfcf0bfbd7bed1
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-03 16:09:51 +0300

    C# MySQL

---
+++ MySQLAPP/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+using MySQLAPP.DAOs;
+using MySQLAPP.Models;
+
+namespace MySQLAPP // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         AnimalsDAO query= new AnimalsDAO();
+            //Animals animal = new Animals() { Name = "орел", Type = "птица" };
+            //Console.WriteLine(query.Add(animal));
+            List<Animals> list = new List<Animals>
+            {
+                new Animals() { Name = "воробей", Type = "bird" },
+                new Animals() { Name = "синица", Type = "bird" },
+                new Animals() { Name = "латсочка", Type = "bird" },
+                new Animals() { Name = "хомочек", Type = "млекопетающие" },
+                new Animals() { Name = "дятел", Type = "bird" }
+            };
+            query.AddRange(list);
+
+            Console.ReadKey();
+
+        }
+    }
+}
