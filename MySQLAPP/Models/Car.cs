7ce8d47df5e7ce1dc050c1df83fcef23f9b97f17
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-06 22:55:07 +0300

    CarClass

---
+++ MySQLAPP/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQLAPP.Models
+{
+    public  class Car
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public DateTime TimeCreate { get; set; }
+        public string Type { get; set; }
+        public double Mileage { get; set; }
+
+        public override string ToString()
+        {
+            return $"код: {Code} Название: {Name}  Цвет: {Color}  Дата: {TimeCreate}  Тип кузова: {Type}  Пробег: {Mileage} миль";
+        }
+
+    }
+}
