395ddc3edc37ceac346feb34089c4e37d6e19f1a
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-07 18:32:00 +0300

    update

--- MySQLAPP/DAOs/CarsDao.cs
+++ MySQLAPP/DAOs/CarsDao.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using MySQLAPP.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,8 @@ namespace MySQLAPP.DAOs
     {
         private readonly string ConnectionString = "server=localhost;database=studies;uid=root;password=123qwe4r5t6YY;";
         //private readonly string SQL_AddItem = "insert into Car ('code','name','color','datecreate','type','mileage') values {0};";
-        private readonly string SQL_AddItem = "insert into Car ('code','name','color','type','mileage') values {0};";
+        private readonly string SQL_AddItem = "insert into Car ('code','name','color','type','mileage') values (@code1, @name1, @color1, @type1, @mileage);";
+        private readonly string SQL_selectItems = "select code,name,color,datecreate,type,mileage from car;";
 
         public int Add(Car car)
         {
@@ -22,13 +24,13 @@ namespace MySQLAPP.DAOs
             try
             {
                 //MySqlCommand command=new MySqlCommand(string.Format(SQL_AddItem,"(@code1, @name1, @color1, @datacreate1, @type1, @mileage)"), connection);
-                MySqlCommand command = new MySqlCommand(string.Format(SQL_AddItem, "(@code1, @name1, @color1, @type1, @mileage)"), connection);
+                MySqlCommand command = new MySqlCommand(SQL_AddItem, connection);
                 command.Parameters.AddWithValue("@code1", car.Code);
                 command.Parameters.AddWithValue("@name1", car.Name);
                 command.Parameters.AddWithValue("@color1", car.Color);
-                //command.Parameters.AddWithValue("@datecreate1", car.TimeCreate);
+                command.Parameters.AddWithValue("@datecreate1", car.TimeCreate);
                 command.Parameters.AddWithValue("@type1", car.Type);
-                command.Parameters.AddWithValue("@mileage1", car.Mileage);
+                command.Parameters.AddWithValue("@mileage1", (double)car.Mileage);
                 command.ExecuteNonQuery();
                 return (int)command.LastInsertedId;
             }
@@ -43,6 +45,47 @@ namespace MySQLAPP.DAOs
             }
         }
 
+        public List<Car> GetCars()
+        {
+
+            MySqlConnection connection = Connection();
+            if (connection == null) throw new Exception("connection error");
+            try
+            {
+                List<Car> cars = new List<Car>();
+                MySqlCommand command = new MySqlCommand(SQL_selectItems, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Car car = new Car();
+                    car.Code = reader.GetInt32(0);
+                    car.Name = reader.GetString(1);
+                    car.Color = reader.GetString(2);
+                    car.TimeCreate=reader.GetString(3);
+                    car.Type = reader.GetString(4);
+                    car.Mileage = reader.GetDouble(5);
+                    cars.Add(car);
+                }
+                return cars;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public void Show(CarsDao cars)
+        {
+            foreach (var item in cars.GetCars())
+            {
+                Console.WriteLine(item);
+            }
+        }
 
 
 
