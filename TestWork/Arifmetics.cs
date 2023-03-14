c6f488c05c1fbeb56bc2274c4dec419b5ffb08b0
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-14 23:52:39 +0300

    new work

---
+++ TestWork/Arifmetics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWork
+{
+    public static class Arifmetics
+    {
+        public static double Sum(string x, string y)
+        {
+            if (double.TryParse(x, out double resultX) && double.TryParse(y, out double resultY))
+            {
+                return resultX + resultY;
+            }
+
+            return 0;
+
+
+        }
+    }
+}
---
+++ TestWork/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        Console.WriteLine(Sum(22, 32));
+        }
+    }
