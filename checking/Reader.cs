550df6b188524451713a87ade1bf86da4c5628ac
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-27 15:00:05 +0300

    update

--- checking/Reader.cs
+++ checking/Reader.cs
@@ -4,6 +4,7 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
@@ -16,11 +17,12 @@ namespace checking
         private readonly string ConnectionString = "server=localhost;database=studies;uid=root;password=123qwe4r5t6YY;";
         private readonly string SQL_AddDataBase = "create database if not exists Scripts;";
         private readonly string SQL_UseDatabase= "use Scripts;";
-        private readonly string SQL_CreateTable= "create table if not exists TableScripts (\r\n Id INT auto_increment not null,\r\n Name varchar(55) not null,\r\n primary key(id)\r\n );";
+        private readonly string SQL_CreateTable= "create table if not exists TableScripts (\r\n Id INT auto_increment not null,\r\n Name varchar(255) not null,\r\n primary key(id)\r\n );";
         private readonly string SQL_selectItems = "select name from TableScripts;";
+        private readonly string SQL_AddItem = "insert into TableScripts (name) values (@name1);";
 
-        // метод, который считает все названия скриптов
-        public List<string> GetScripts()
+        // метод, который считает все названия скриптов с базы данных
+        public List<string> GetScriptsName()
         {
             MySqlConnection connection = Connection();
             if (connection == null) throw new Exception("connection error");
@@ -45,18 +47,50 @@ namespace checking
                 connection.Close();
             }
         }
+        // метод считывания названий файлов в папке
+        public List<string> ListNames()
+        {
+            string mypath = "D:\\ДЗ С#\\hschool\\hschool_beggining_csh\\OOPLearning\\checking\\Scripts";
+            List<string> listAllFileNames = new List<string>();
+            listAllFileNames.AddRange(Directory.GetFiles(mypath, "*.sql", SearchOption.TopDirectoryOnly));
+            return listAllFileNames;
+        }
+
+        
 
         // метод, который сравнит названия,если нет его в списке,то вызывает метод считывания и его выполнение, затем добавление записи в базу данных
 
+        //метод добавления записи в базу данных
+        void AddScriptInTable(string scr)
+        {
+            if (scr == null) throw new ArgumentNullException(nameof(scr));
+            MySqlConnection connection = Connection();
+            if (connection == null) throw new Exception("Connection Error");
+            try
+            {
+                MySqlCommand command = new MySqlCommand(SQL_AddItem, connection);
+                command.Parameters.AddWithValue("@name1", scr);
+                command.ExecuteNonQuery();
+               
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
 
 
 
-        //
-        public void ChekerScript(string path, string fileChecker)
+        static void ChekerScript(string path, string fileChecker)
         {
             List<string> list = new List<string>();
             //Directory.GetFiles(mypath, "*.txt", SearchOption.TopDirectoryOnly).ToList().ForEach(f => Console.WriteLine(Path.GetFileName(f)));
-            string[] allfiles = Directory.GetFiles(path, "*.txt", SearchOption.TopDirectoryOnly);
+            string[] allfiles = Directory.GetFiles(path, "*.sql", SearchOption.TopDirectoryOnly);
             foreach (string filename in allfiles)
             {
                 list.Add(Path.GetFileName(filename));
