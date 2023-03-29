c3773f0baf71850c2d2d6424d954e3b20f175712
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-29 03:00:50 +0300

    clear code

--- checking/Reader.cs
+++ checking/Reader.cs
@@ -176,39 +176,7 @@ namespace checking
         //        return null;
         //    }
         //}
-        //static void ChekerScript(string path, string fileChecker)
-        //{
-        //    List<string> list = new List<string>();
-        //    //Directory.GetFiles(mypath, "*.txt", SearchOption.TopDirectoryOnly).ToList().ForEach(f => Console.WriteLine(Path.GetFileName(f)));
-        //    string[] allfiles = Directory.GetFiles(path, "*.sql", SearchOption.TopDirectoryOnly);
-        //    foreach (string filename in allfiles)
-        //    {
-        //        list.Add(Path.GetFileName(filename));
-
-        //    }
-        //    string[] listec = File.ReadAllLines(fileChecker);
-        //    foreach (string line in listec)
-        //    {
-        //        if (!list.Contains(line))
-        //        {
-        //            //запуск этого скрипта к выполнению
-        //            //добавление названия скрипта в файл со списком скриптов Files.txt
-        //           // File.AppendAllText(fileChecker, line);                                // открывает наш файл, добавляет в него строку
-
-        //        }
-
-        //    }
-        //}
-        //    public void LoaderApp(string str)
-        //{
-        //    List<string> list = new List<string>();            
-        //    string[] allfiles = Directory.GetFiles(str, "*.txt", SearchOption.TopDirectoryOnly);
-        //    foreach (string filename in allfiles)
-        //    {
-        //        list.Add(Path.GetFileName(filename));
-        //        Process.Start(filename);
-        //    }
-        //}
+       
 
     }
 
