e9f2017d65ee034b3c1e9ce76c6703aefaf2edff
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-02 20:12:04 +0300

    proto

---
+++ Pattern/PatterPrototype.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    public interface IAnimal
+    {
+        //void SetName(string name);
+        //string GetName();
+        IAnimal Clone();
+    }
+    public class PatterPrototype : IAnimal
+    {
+        private string name;
+        public PatterPrototype() { }
+        private PatterPrototype(PatterPrototype donor) => name = donor.name;
+        public void SetName(string name) => this.name = name;
+        public string GetName() => name;
+        public IAnimal Clone() => new PatterPrototype(this);
+
+    }
+}
