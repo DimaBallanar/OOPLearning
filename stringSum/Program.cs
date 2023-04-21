e073edb9490b444c451ccf1b1ccda4f0aa21f364
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-21 20:21:47 +0300

    update

--- stringSum/Program.cs
+++ stringSum/Program.cs
@@ -6,15 +6,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            StringC str= new StringC();
-            string ops = str.InnerArray();
-            foreach (var op in ops) 
-            {
-                Console.Write(op);
-            }
-            Console.WriteLine();
-            str.MakeLeftSide();
-            str.MakeRightSides();
+            //StringC str= new StringC();
+            //string ops = str.InnerArray();
+            //foreach (var op in ops) 
+            //{
+            //    Console.Write(op);
+            //}
+            //Console.WriteLine();
+            //str.MakeLeftSide();
+            //str.MakeRightSides();
+            Code coe=new Code();
+            coe.Array();
+            //foreach(string x in coe.Array()) { Console.WriteLine(x); }
+            Console.WriteLine(coe.Array()[1]);
         }
     }
 }
--- stringSum/StringC.cs
+++ stringSum/StringC.cs
@@ -13,7 +13,7 @@ namespace stringSum
         public static string Text = "123+1*22-17 /2+  22- 19+33.2 * 124 -11+ 22- 14.9 *2  -1";
 
         public static string Pattern = "*/+-";
-
+        public char CentralSymbol;
         public string InnerArray()
         {
             string resultArray = Text.Replace(" ", "");//.Split("");    // чистим все пробелы и массив забиваем
@@ -25,7 +25,7 @@ namespace stringSum
             string text = MakeLeftSide();
             string RigthPart = "";
 
-            for (int i = text.Length; i < textAll.Length; i++)
+            for (int i = text.Length+1; i < textAll.Length; i++)
             {
                 RigthPart += textAll[i];
 
@@ -53,7 +53,7 @@ namespace stringSum
 
             foreach (char c in text)
             {
-                if (counter > 0)
+                if (counter >= 0)
                 {
                     if (Pattern.Contains(c))
                     {
@@ -65,17 +65,25 @@ namespace stringSum
                         LeftPart += c;
                     }
                 }
-                else
-                {
-                    Console.WriteLine($"{LeftPart}   это левая часть");
-                    return LeftPart; }
+                //else
+                //{
+                //    Console.WriteLine($"{LeftPart}   это левая часть");
+                //    return LeftPart;
+                //}
             }
-
+            CentralSymbol = LeftPart[LeftPart.Length - 1];
+            LeftPart = LeftPart.Remove(LeftPart.Length - 1);
             Console.WriteLine($"{LeftPart}   это левая часть");
             return LeftPart;
         }
+        // надо дернуть последний арифметический знак с левой части              *****DONE   CentralSymbol
+        
+        // нужна логика дробления по элементам куска стрингов
+        public void Droubl(string text)  
+        {
+            string nabor= "123 + 1 * 22 - 17 / 2 + 22 - 19";
 
-
+        }
 
 
 
