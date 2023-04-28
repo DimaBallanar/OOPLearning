634198e90d6aaadba2542f43ae6283ec5a1bac21
Dima Ballanar <D.Ballanar@gmail.com>
2023-04-28 19:12:27 +0300

    put user:

--- OnlineShop/Controllers/UserController.cs
+++ OnlineShop/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models;
 using OnlineShop.Repository;
 
 namespace OnlineShop.Controllers
@@ -12,12 +13,40 @@ namespace OnlineShop.Controllers
         {
             m_userRepository = userRepository;
         }
+
+        //https://localhost:7172/api/User
         [HttpGet]
         public IActionResult Get()
         {
             try
             {
                 return Ok(m_userRepository.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        //https://localhost:7172/api/User/get/1
+        [HttpGet("get/{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(m_userRepository.GetAll(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(User user) 
+        {
+            try
+            {
+                return Ok(m_userRepository.Put(user));
             }
             catch (Exception ex)
             {
--- OnlineShop/Repository/UserRepository.cs
+++ OnlineShop/Repository/UserRepository.cs
@@ -7,6 +7,7 @@ namespace OnlineShop.Repository
     {
         private readonly MySqlConnection m_Connection;
         private readonly string SQL_SELECT_GET_ALL = "Select id,name,surname,email,password from studaki";
+        private readonly string SQL_PUT_ITEM="insert into studaki(name,surname,email,password) values (@name, @surname, @email, @password)";
         public UserRepository(MySqlConnection connection)
         {
             m_Connection = connection;
@@ -61,6 +62,43 @@ namespace OnlineShop.Repository
                     }
                 }
                 return users;
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+
+        public int Put(User user)
+        {
+            try
+            {
+                m_Connection.Open();
+                MySqlCommand cmd = new MySqlCommand(SQL_SELECT_GET_ALL, m_Connection);
+               
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader.GetString(3) != user.Email)
+                    {
+                        m_Connection.Close();
+                        User user1 = new User();
+                        m_Connection.Open();
+                        MySqlCommand command=new MySqlCommand(SQL_PUT_ITEM, m_Connection);
+                        //MySqlCommand command = m_Connection.CreateCommand();
+                        //command.CommandText = SQL_PUT_ITEM;
+                        command.Parameters.AddWithValue("@name", user.Name);
+                        command.Parameters.AddWithValue("@surname", user.Surname);
+                        command.Parameters.AddWithValue("@email", user.Email);
+                        command.Parameters.AddWithValue("@password", user.Password);
+                        
+                        //cmd.ExecuteNonQuery();
+                        command.ExecuteNonQuery();
+                        return (int)command.LastInsertedId;
+                    }
+                    
+                }
+                return -1;
             }
             catch (MySqlException e)
             {
