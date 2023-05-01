66dae4a2fff48bb616b07d77f22b6b486ed1b025
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-01 17:05:34 +0300

    brand update

---
+++ OnlineShop/Models/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineShop.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
---
+++ OnlineShop/Repository/BrandRepository.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using OnlineShop.Models;
+
+namespace OnlineShop.Repository
+{
+    public class BrandRepository : BaseRepository
+    {
+        private readonly string SQL_SELECT_GET_ALL = "Select id,name from brand";
+        private readonly string SQL_PUT_ITEM = "insert into brand(name) values (@name)";
+        private readonly string SQL_UPDATE_USER = "UPDATE brand Set name=@name where Id={0}";
+        private readonly string SQL_DELETE_USER = "delete from brand where Id=@id;";
+
+        public BrandRepository(MySqlConnection connection) : base(connection)
+        {
+
+        }
+        public List<User> GetAll()
+        {
+            try
+            {
+                m_Connection.Open();
+                MySqlCommand cmd = new MySqlCommand(SQL_SELECT_GET_ALL, m_Connection);
+                List<User> users = new List<User>();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    users.Add(new User()
+                    {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1)                        
+                    });
+                }
+                return users;
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+    }
+}
