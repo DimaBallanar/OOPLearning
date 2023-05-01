abdaa034decf447ef45d983bd34303a605bbb08c
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-01 17:22:16 +0300

    update

--- OnlineShop/Repository/BrandRepository.cs
+++ OnlineShop/Repository/BrandRepository.cs
@@ -7,8 +7,8 @@ namespace OnlineShop.Repository
     {
         private readonly string SQL_SELECT_GET_ALL = "Select id,name from brand";
         private readonly string SQL_PUT_ITEM = "insert into brand(name) values (@name)";
-        private readonly string SQL_UPDATE_USER = "UPDATE brand Set name=@name where Id={0}";
-        private readonly string SQL_DELETE_USER = "delete from brand where Id=@id;";
+        private readonly string SQL_UPDATE_BRAND = "UPDATE brand Set name=@name where Id={0}";
+        private readonly string SQL_DELETE_BRAND = "delete from brand where Id=@id;";
 
         public BrandRepository(MySqlConnection connection) : base(connection)
         {
@@ -38,26 +38,26 @@ namespace OnlineShop.Repository
             }
         }
 
-        public List<User> GetAll(int id)
+        public List<Brand> GetAll(int id)
         {
             try
             {
                 m_Connection.Open();
                 MySqlCommand cmd = new MySqlCommand(SQL_SELECT_GET_ALL, m_Connection);
-                List<User> users = new List<User>();
+                List<Brand> brands = new List<Brand>();
                 MySqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
                     if (reader.GetInt32(0) == id)
                     {
-                        users.Add(new User()
+                        brands.Add(new Brand()
                         {
                             Id = reader.GetInt32(0),
                             Name = reader.GetString(1)                           
                         });
                     }
                 }
-                return users;
+                return brands;
             }
             catch (MySqlException e)
             {
@@ -74,7 +74,7 @@ namespace OnlineShop.Repository
                 MySqlDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                 {
-                    if (reader.GetString(3) != brand.Name)
+                    if (reader.GetString(1) != brand.Name)
                     {
                         m_Connection.Close();
                         Brand brand1 = new Brand();
@@ -91,6 +91,44 @@ namespace OnlineShop.Repository
             catch (MySqlException e)
             {
                 throw e;
+            }
+        }
+
+        public List<Brand> Update(Brand brand)
+        {
+            try
+            {
+                m_Connection.Open();
+                MySqlCommand cmd = new MySqlCommand(string.Format(SQL_UPDATE_BRAND, brand.Id), m_Connection);
+                cmd.Parameters.AddWithValue("@name", brand.Name);               
+                cmd.ExecuteNonQuery();
+                m_Connection.Close();
+                List<Brand> brands = GetAll();
+                return brands;
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+
+        public List<Brand> Delete(int code)
+        {
+            try
+            {
+                m_Connection.Open();
+
+                MySqlCommand command = new MySqlCommand(SQL_DELETE_BRAND, m_Connection);
+                command.Parameters.AddWithValue("@id", code);
+                command.ExecuteNonQuery();
+                m_Connection.Close();
+                List<Brand> brands = GetAll();
+                return brands;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
             }
         }
     }
