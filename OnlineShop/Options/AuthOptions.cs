58f1a8ec56c4c737693ad4813a51873f8aab3f2d
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-07 15:56:29 +0300

    shop

--- OnlineShop/Options/AuthOptions.cs
+++ OnlineShop/Options/AuthOptions.cs
@@ -4,8 +4,8 @@ using System.Text;
 namespace Options;
 public class AuthOptions
 {
-    public const string ISSUER = "MyAuthServer"; // издатель токена
-    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    public const string ISSUER = "MyAuthServer";   // издатель токена
+    public const string AUDIENCE = "MyAuthClient";   // потребитель токена
     const string KEY = "mysupersecret_secretkey!123"; // ключ для шифрации
     public const int LIFETIME = 10; // время жизни токена - 10 минут
     public static SymmetricSecurityKey GetSymmetricSecurityKey()
