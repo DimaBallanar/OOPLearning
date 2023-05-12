d71990cf7ac5b66008579af7130c71a59b10ad1d
Dima Ballanar <D.Ballanar@gmail.com>
2023-05-12 15:57:52 +0300

    Extensions

---
+++ Extensions/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Extensions;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "12no31b2hgw1231";
+            Console.WriteLine(text);
+            string newText = text.DecorateSymbol('$');
+            Console.WriteLine(newText);
+        }
+    }
+}
---
+++ Extensions/StringExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    internal static class StringExtension
+    {
+        public static string DecorateSymbol(this string str, char symbol)
+        {
+            StringBuilder builder= new StringBuilder();
+            foreach(var ch in str)
+            {
+                builder.Append(ch);
+                if(char.IsDigit(ch))
+                {
+                    builder.Append(symbol);
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
